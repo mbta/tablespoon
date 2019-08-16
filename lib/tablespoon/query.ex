a2bba77b931b8d8b0dbacde1a59456c84ad3a6fe
Paul Swartz <paul@paulswartz.net>
2019-08-16 18:42:33 -0400

    feat: initial Query struct
    Create a struct to track the relevant pieces of data for a TSP
    request/cancel. We store `received_at` as a monotonic time for tracking the
    internal processing of the request.

---
+++ lib/tablespoon/query.ex
@@ -0,0 +1,38 @@
+defmodule Tablespoon.Query do
+  @moduledoc """
+  A message from a vehicle to either request or cancel priority.
+  """
+  @enforce_keys [
+    :id,
+    :type,
+    :vehicle_id,
+    :intersection_alias,
+    :approach,
+    :event_time,
+    :received_at_mono
+  ]
+  defstruct @enforce_keys
+
+  @type t :: %__MODULE__{
+          id: id,
+          type: query_type,
+          vehicle_id: vehicle_id,
+          intersection_alias: intersection_alias,
+          approach: approach,
+          event_time: non_neg_integer,
+          received_at_mono: integer
+        }
+  @type id :: binary
+  @type query_type :: :request | :cancel
+  @type vehicle_id :: binary
+  @type intersection_alias :: binary
+  @type approach :: :north | :east | :south | :west
+
+  @doc "Create a new Query"
+  @spec new(Keyword.t()) :: t
+  def new(opts) do
+    opts = Map.new(opts)
+    opts = Map.put(opts, :received_at_mono, System.monotonic_time())
+    struct!(__MODULE__, opts)
+  end
+end
---
+++ test/tablespoon/query_test.exs
@@ -0,0 +1,25 @@
+defmodule Tablespoon.QueryTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  import Tablespoon.Query
+
+  describe "new/1" do
+    test "sets received_at_mono" do
+      query =
+        new(
+          id: "id",
+          type: :request,
+          vehicle_id: "veh",
+          intersection_alias: "int",
+          approach: :west,
+          event_time: System.system_time()
+        )
+
+      assert is_integer(query.received_at_mono)
+    end
+
+    test "will not build a struct with missing values" do
+      assert_raise ArgumentError, fn -> new([]) end
+    end
+  end
+end
