4c6408708b2d9f313c27c5de69f378b8b2432aac
Paul Swartz <paul@paulswartz.net>
2019-08-17 11:17:39 -0400

    feat: parse monitoring times into Config
    We'll use these to have an Intersection log a warning if it hasn't received a message.

--- lib/tablespoon/intersection/config.ex
+++ lib/tablespoon/intersection/config.ex
@@ -2,11 +2,15 @@ defmodule Tablespoon.Intersection.Config do
   @moduledoc """
   Configuration options for Intersections.
   """
+  @enforce_keys [:id, :alias]
   defstruct [
     :id,
-    :name,
     :alias,
-    :active?
+    name: nil,
+    active?: true,
+    warning_timeout_ms: :infinity,
+    warning_not_before_time: {24, 0, 0},
+    warning_not_after_time: {0, 0, 0}
   ]
 
   @doc "Parse a JSON object into a Config"
@@ -15,9 +19,37 @@ defmodule Tablespoon.Intersection.Config do
       "id" => id,
       "name" => name,
       "intersectionAlias" => intersection_alias,
-      "active" => active?
+      "active" => active?,
+      "monitoringInterval" => warning_timeout_minute,
+      "startTime" => warning_not_before_binary,
+      "endTime" => warning_not_after_binary
     } = map
 
-    %__MODULE__{id: id, name: name, alias: intersection_alias, active?: active?}
+    warning_timeout_ms = warning_timeout_minute * 60_000
+    warning_not_before = time(warning_not_before_binary)
+
+    warning_not_after =
+      case time(warning_not_after_binary) do
+        {0, 0, 0} -> {24, 0, 0}
+        time -> time
+      end
+
+    %__MODULE__{
+      id: id,
+      name: name,
+      alias: intersection_alias,
+      active?: active?,
+      warning_timeout_ms: warning_timeout_ms,
+      warning_not_before_time: warning_not_before,
+      warning_not_after_time: warning_not_after
+    }
+  end
+
+  defp time(<<hour_binary::binary-2, ?:, minute_binary::binary-2, ?:, second_binary::binary-2>>) do
+    {
+      String.to_integer(hour_binary),
+      String.to_integer(minute_binary),
+      String.to_integer(second_binary)
+    }
   end
 end
--- test/tablespoon/intersection/config_test.exs
+++ test/tablespoon/intersection/config_test.exs
@@ -7,12 +7,24 @@ defmodule Tablespoon.Intersection.ConfigTest do
     "id" => 1,
     "name" => "TSP Intersection",
     "intersectionAlias" => "BOS123",
-    "active" => true
+    "active" => true,
+    "monitoringInterval" => 240,
+    "startTime" => "07:00:00",
+    "endTime" => "00:00:00"
   }
 
   describe "from_json" do
     test "returns a %Config{}" do
-      expected = %Config{id: 1, name: "TSP Intersection", alias: "BOS123", active?: true}
+      expected = %Config{
+        id: 1,
+        name: "TSP Intersection",
+        alias: "BOS123",
+        active?: true,
+        warning_timeout_ms: 240 * 60 * 1000,
+        warning_not_before_time: {7, 0, 0},
+        warning_not_after_time: {24, 0, 0}
+      }
+
       actual = from_json(@sample_json)
       assert expected == actual
     end
