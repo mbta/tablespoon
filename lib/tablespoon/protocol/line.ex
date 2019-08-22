1de504eea9ff3b9c925a632009c8dc2893f97ab2
Paul Swartz <paul@paulswartz.net>
2019-08-22 13:55:22 -0400

    feat: simple line protocol
    We'll use this for sending/receiving Hayes modem commands over SSH in the future.

---
+++ lib/tablespoon/protocol/line.ex
@@ -0,0 +1,28 @@
+defmodule Tablespoon.Protocol.Line do
+  @moduledoc """
+  Implementation of a simple line-based protocol.
+
+  Accepts any line ending in \n with 0 or more \r beforehand.
+  """
+
+  @type error :: :too_short
+
+  @suffix "\n"
+
+  @doc "Encode a line and the suffix"
+  def encode(line) when is_binary(line) do
+    line <> @suffix
+  end
+
+  @doc "Decode a line and any extra data"
+  @spec decode(binary) :: {:ok, binary, binary} | {:error, error}
+  def decode(binary) when is_binary(binary) do
+    case :binary.split(binary, @suffix) do
+      [line, rest] ->
+        {:ok, String.trim_trailing(line, "\r"), rest}
+
+      [_] ->
+        {:error, :too_short}
+    end
+  end
+end
---
+++ test/tablespoon/protocol/line_test.exs
@@ -0,0 +1,41 @@
+defmodule Tablespoon.Protocol.LineTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  use ExUnitProperties
+  import Tablespoon.Protocol.Line
+
+  describe "encode/decode" do
+    property "returns the same binary" do
+      check all contents <- contents() do
+        assert decode(encode(contents)) == {:ok, contents, ""}
+      end
+    end
+  end
+
+  describe "decode" do
+    property "handles any number of extra carriage returns or extra data" do
+      check all contents <- contents(),
+                end_of_line <- end_of_line(),
+                extra <- StreamData.list_of(StreamData.one_of([contents(), end_of_line()])) do
+        assert decode(IO.iodata_to_binary([contents, end_of_line, extra])) ==
+                 {:ok, contents, IO.iodata_to_binary(extra)}
+      end
+    end
+
+    property "returns {:error, :too_short} without a newline" do
+      check all contents <- contents() do
+        assert decode(contents) == {:error, :too_short}
+      end
+    end
+  end
+
+  def contents do
+    StreamData.string(?\s..?Z)
+  end
+
+  def end_of_line do
+    [?\r]
+    |> StreamData.string()
+    |> StreamData.map(&(&1 <> "\n"))
+  end
+end
