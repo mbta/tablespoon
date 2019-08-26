c9a62b20a32855c27844f633b0a46008d9cea4d1
Paul Swartz <paul@paulswartz.net>
2019-08-26 13:24:35 -0400

    feat(Transport): add generic functions for calling out to an impl
    This way, the communicator can be agnostic to the transport, which we'll take
    advantage of in testing.

--- lib/tablespoon/transport.ex
+++ lib/tablespoon/transport.ex
@@ -11,4 +11,19 @@ defmodule Tablespoon.Transport do
   @callback connect(t) :: {:ok, t} | {:error, error}
   @callback stream(t, term) :: {:ok, t, [result]} | {:error, error} | :unknown
   @callback send(t, iodata) :: {:ok, t} | {:error, error}
+
+  @doc "Call connect/1 on the implementation struct"
+  def connect(struct) do
+    struct.__struct__.connect(struct)
+  end
+
+  @doc "Call stream/2 on the implementation struct"
+  def stream(struct, message) do
+    struct.__struct__.stream(struct, message)
+  end
+
+  @doc "Call send/2 on the implementation struct"
+  def send(struct, iodata) do
+    struct.__struct__.send(struct, iodata)
+  end
 end
