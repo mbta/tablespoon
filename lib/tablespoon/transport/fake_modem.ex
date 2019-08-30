c53e1f8a3872482181da624af8570c600c16a680
Paul Swartz <paul@paulswartz.net>
2019-08-30 14:09:12 -0400

    feat(Transport.FakeModem): provide configuration for chaos

--- lib/tablespoon/transport/fake_modem.ex
+++ lib/tablespoon/transport/fake_modem.ex
@@ -2,47 +2,79 @@ defmodule Tablespoon.Transport.FakeModem do
   @moduledoc """
   Transport implementation which pretends to be a modem.
 
-  It sends an initial "OK\n" line, then responds to any lines it receives with "OK".
+  By default, sends an initial "OK\n" line, then responds to any lines it receives with "OK".
+
+  However, there are some configuration variables that can be set to change that.
+
+  - send_error_rate: the percent of messages which result in a sending error (1 to 100)
+  - disconnect_rate: the percent of replies which result in a disconnection (to 100)
+  - delay_range: a range of milliseconds by which to delay replies
   """
   @behaviour Tablespoon.Transport
 
   alias Tablespoon.Protocol.Line
   require Logger
-  defstruct [:ref, :buffer]
+
+  defstruct [
+    :ref,
+    :buffer,
+    send_error_rate: 0,
+    response_error_rate: 0,
+    disconnect_rate: 0,
+    delay_range: 0..0
+  ]
 
   @impl Tablespoon.Transport
-  def new(_opts \\ []) do
-    %__MODULE__{}
+  def new(opts \\ []) do
+    struct(__MODULE__, opts)
   end
 
   @impl Tablespoon.Transport
   def connect(%__MODULE__{} = t) do
     ref = make_ref()
     t = %{t | ref: ref, buffer: ""}
-    reply_ok(t)
+    reply(t)
     {:ok, t}
   end
 
   @impl Tablespoon.Transport
+  def send(%__MODULE__{ref: nil}, _) do
+    {:error, :not_connected}
+  end
+
   def send(%__MODULE__{} = t, data) do
-    buffer = IO.iodata_to_binary([t.buffer, data])
-    t = %{t | buffer: buffer}
-    handle_buffer(t)
+    if trigger?(t.send_error_rate) do
+      {:error, :trigger_failed}
+    else
+      buffer = IO.iodata_to_binary([t.buffer, data])
+      t = %{t | buffer: buffer}
+      handle_buffer(t)
+    end
   end
 
   @impl Tablespoon.Transport
   def stream(%__MODULE__{ref: ref} = t, {ref, message}) do
-    {:ok, t, [message]}
+    if trigger?(t.disconnect_rate) do
+      t = %{t | ref: nil, buffer: ""}
+      {:ok, t, [:closed]}
+    else
+      {:ok, t, [message]}
+    end
   end
 
   def stream(%__MODULE__{}, _) do
     :unknown
   end
 
-  defp reply_ok(t) do
-    Kernel.send(self(), {t.ref, {:data, "OK"}})
-    Kernel.send(self(), {t.ref, {:data, "\r"}})
-    Kernel.send(self(), {t.ref, {:data, "\n"}})
+  defp reply(t, data \\ "OK") do
+    delay = Enum.random(t.delay_range)
+
+    _ =
+      for message <- [data, "\r", "\n"] do
+        Process.send_after(self(), {t.ref, {:data, message}}, delay)
+      end
+
+    :ok
   end
 
   defp handle_buffer(t) do
@@ -56,12 +88,17 @@ defmodule Tablespoon.Transport.FakeModem do
     end
   end
 
-  defp handle_line(t, "") do
+  defp handle_line(t, "AT*RELAYOUT" <> _) do
+    if trigger?(t.response_error_rate) do
+      reply(t, "ERROR")
+    else
+      reply(t)
+    end
+
     handle_buffer(t)
   end
 
-  defp handle_line(t, "AT*RELAYOUT" <> _) do
-    reply_ok(t)
-    handle_buffer(t)
+  def trigger?(rate) do
+    Enum.random(1..100) <= rate
   end
 end
---
+++ test/tablespoon/transport/fake_modem_test.exs
@@ -0,0 +1,69 @@
+defmodule Tablespoon.Transport.FakeModemTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  alias Tablespoon.Transport.FakeModem
+
+  @packet "AT*RELAYOUT5=0\r\n"
+
+  describe "send/2" do
+    test "sends a data reply" do
+      t = FakeModem.new()
+      {:ok, t} = FakeModem.connect(t)
+      receive_connect_messages(t)
+      assert {:ok, t} = FakeModem.send(t, @packet)
+      assert_receive x
+      assert {:ok, _, [{:data, _}]} = FakeModem.stream(t, x)
+    end
+
+    test "when not connected, returns an error" do
+      t = FakeModem.new()
+      assert {:error, :not_connected} = FakeModem.send(t, @packet)
+    end
+
+    test "when send_error_rate is 100, returns an error" do
+      t = FakeModem.new(send_error_rate: 100)
+      {:ok, t} = FakeModem.connect(t)
+      receive_connect_messages(t)
+      assert {:error, :trigger_failed} = FakeModem.send(t, @packet)
+      refute_receive _
+    end
+
+    test "when response_error_rate is 100, replies with an error" do
+      t = FakeModem.new(response_error_rate: 100)
+      {:ok, t} = FakeModem.connect(t)
+      receive_connect_messages(t)
+      assert {:ok, t} = FakeModem.send(t, @packet)
+      assert_receive x
+      assert {:ok, _, [{:data, "ERROR"}]} = FakeModem.stream(t, x)
+    end
+
+    test "when delay_range is sent, waits to send the reply" do
+      t = FakeModem.new(delay_range: 5..10)
+      {:ok, t} = FakeModem.connect(t)
+      assert {:ok, t} = FakeModem.send(t, @packet)
+      refute_received _
+      # wait...
+      assert_receive x
+      assert {:ok, _, [{:data, _}]} = FakeModem.stream(t, x)
+    end
+  end
+
+  describe "stream/2" do
+    test "when disconnect_rate is 100, always disconnects rather than sending a data response" do
+      t = FakeModem.new(disconnect_rate: 100)
+      {:ok, t} = FakeModem.connect(t)
+      message = {t.ref, {:data, @packet}}
+      assert {:ok, t, [:closed]} = FakeModem.stream(t, message)
+      refute t.ref
+    end
+  end
+
+  defp receive_connect_messages(t) do
+    # we get some initial messages on calling connect/1
+    ref = t.ref
+    assert_receive {^ref, {:data, "OK"}}
+    assert_receive {^ref, {:data, "\r"}}
+    assert_receive {^ref, {:data, "\n"}}
+  end
+end
