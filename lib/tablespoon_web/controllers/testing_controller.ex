cba49405a03cccda67f6f66e36e3e5f2de074240
Paul Swartz <paul@paulswartz.net>
2019-09-05 11:58:00 -0400

    feat: query testing page at /_testing
    This allows us to easily send sample requests.

---
+++ lib/tablespoon_web/controllers/testing_controller.ex
@@ -0,0 +1,10 @@
+defmodule TablespoonWeb.Controllers.Testing do
+  @moduledoc """
+  Renders a view to help with testing.
+  """
+  use TablespoonWeb, :controller
+
+  def index(conn, _params) do
+    send_file(conn, 200, "priv/testing.html")
+  end
+end
--- lib/tablespoon_web/router.ex
+++ lib/tablespoon_web/router.ex
@@ -2,4 +2,5 @@ defmodule TablespoonWeb.Router do
   use TablespoonWeb, :router
 
   get "/priority", TablespoonWeb.Controllers.Priority, :index
+  get "/_testing", TablespoonWeb.Controllers.Testing, :index
 end
