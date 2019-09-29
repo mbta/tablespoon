e27530eb5f5f0de2e27bcba386fb03a85225701f
Paul Swartz <paul@paulswartz.net>
2019-09-29 12:55:21 -0400

    test: also allow configuring stream_data max runs via environment
    This will allow us to scale the number of runs to how long the tests are
    taking without needing a code change.

--- config/test.exs
+++ config/test.exs
@@ -11,5 +11,11 @@ config :logger, level: :warn
 
 # Configure StreamData to use more rounds in CI
 if System.get_env("CI") do
-  config :stream_data, max_runs: 500
+  max_runs =
+    case System.get_env("STREAM_DATA_MAX_RUNS") do
+      nil -> 500
+      value -> String.to_integer(value)
+    end
+
+  config :stream_data, max_runs: max_runs
 end
