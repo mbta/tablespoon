6d6e4f5d01e6c8e6f10e907ac158dcd7d29e0e03
Paul Swartz <paul@paulswartz.net>
2019-11-12 14:47:02 -0500

    fix: don't crash on invalid priority requests
    Instead, we have `query_from_params/1` return either {:ok, query} or :error,
    and use a `case` statement to either return a 200 or 400 response.

--- lib/tablespoon_web/controllers/priority_controller.ex
+++ lib/tablespoon_web/controllers/priority_controller.ex
@@ -25,54 +25,72 @@ defmodule TablespoonWeb.PriorityController do
   alias Tablespoon.Intersection
 
   def index(conn, params) do
-    params
-    |> query_from_params
-    |> Intersection.send_query()
+    case query_from_params(params) do
+      {:ok, q} ->
+        Intersection.send_query(q)
+        send_resp(conn, 200, "")
 
-    send_resp(conn, 200, "")
+      :error ->
+        send_resp(conn, 400, "invalid request")
+    end
   end
 
   @doc "Build a Tablespoon.Query from the given params"
+  @spec query_from_params(map) :: {:ok, Tablespoon.Query.t()} | :error
   def query_from_params(params) do
     # include parsing time in the total time
     received_at_mono = System.monotonic_time()
 
-    %{
-      "messageid" => id,
-      "type" => type_binary,
-      "intersection" => intersection_alias,
-      "approach" => approach_binary,
-      "vehicle" => vehicle_id,
-      "t" => event_time_binary
-      # ref is ignored
-    } = params
+    with %{
+           "messageid" => id,
+           "type" => type_binary,
+           "intersection" => intersection_alias,
+           "approach" => approach_binary,
+           "vehicle" => vehicle_id,
+           "t" => event_time_binary
+           # ref is ignored
+         } <- params,
+         {:ok, type} <- type(type_binary),
+         {:ok, approach} <- approach(approach_binary),
+         {:ok, event_time} <- event_time(event_time_binary) do
+      q =
+        Tablespoon.Query.new(
+          id: id,
+          type: type,
+          intersection_alias: intersection_alias,
+          approach: approach,
+          vehicle_id: vehicle_id,
+          event_time: event_time,
+          received_at_mono: received_at_mono
+        )
 
-    type = type(type_binary)
-    approach = approach(approach_binary)
-    event_time = event_time(event_time_binary)
+      {:ok, q}
+    else
+      m when is_map(m) ->
+        :error
 
-    Tablespoon.Query.new(
-      id: id,
-      type: type,
-      intersection_alias: intersection_alias,
-      approach: approach,
-      vehicle_id: vehicle_id,
-      event_time: event_time,
-      received_at_mono: received_at_mono
-    )
+      error ->
+        error
+    end
   end
 
-  defp type("request"), do: :request
-  defp type("cancel"), do: :cancel
+  defp type("request"), do: {:ok, :request}
+  defp type("cancel"), do: {:ok, :cancel}
+  defp type(_), do: :error
 
-  defp approach("1"), do: :north
-  defp approach("2"), do: :east
-  defp approach("3"), do: :south
-  defp approach("4"), do: :west
+  defp approach("1"), do: {:ok, :north}
+  defp approach("2"), do: {:ok, :east}
+  defp approach("3"), do: {:ok, :south}
+  defp approach("4"), do: {:ok, :west}
+  defp approach(_), do: :error
 
   defp event_time(binary) do
-    binary
-    |> String.to_integer()
-    |> System.convert_time_unit(:second, :native)
+    case Integer.parse(binary) do
+      {seconds, ""} ->
+        {:ok, System.convert_time_unit(seconds, :second, :native)}
+
+      _ ->
+        :error
+    end
   end
 end
--- test/tablespoon_web/controllers/priority_controller_test.exs
+++ test/tablespoon_web/controllers/priority_controller_test.exs
@@ -1,13 +1,27 @@
 defmodule TablespoonWeb.PriorityControllerTest do
   @moduledoc false
-  use ExUnit.Case, async: true
+  use TablespoonWeb.ConnCase, async: true
   use ExUnitProperties
+  import ExUnit.CaptureLog
+  import TablespoonWeb.Router.Helpers
   import TablespoonWeb.PriorityController
+
+  describe "index" do
+    test "invalid requests do not log an error", %{conn: conn} do
+      log =
+        capture_log(fn ->
+          conn = get(conn, priority_path(conn, :index))
+          assert conn.status == 400
+        end)
+
+      assert log == ""
+    end
+  end
 
   describe "query_from_params/1" do
     property "params are parsed into a query" do
       check all(params <- gen_params()) do
-        actual = query_from_params(params)
+        {:ok, actual} = query_from_params(params)
         assert %Tablespoon.Query{} = actual
         assert actual.id == params["messageid"]
         assert actual.type == String.to_existing_atom(params["type"])
