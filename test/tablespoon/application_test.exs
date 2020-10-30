06b13d020ca13811b1b111121f9d5b1d41e17c3b
Paul Swartz <paul@paulswartz.net>
2020-10-30 10:09:16 -0400

    feat: support loading interesections from environment
    This way we can store the config in AWS Secrets Manager for deploying to ECS.

--- test/tablespoon/application_test.exs
+++ test/tablespoon/application_test.exs
@@ -4,4 +4,27 @@ defmodule Tablespoon.ApplicationTest do
   import Tablespoon.Application
 
   doctest Tablespoon.Application
+
+  describe "configs/1" do
+    test "returns parsed JSON if present" do
+      json = [
+        %{
+          "name" => "name",
+          "intersectionAlias" => "alias",
+          "communicationType" => "Btd",
+          "intersectionId" => 0,
+          "active" => false,
+          "monitoringInterval" => 0,
+          "startTime" => "00:00:00",
+          "endTime" => "11:59:59"
+        }
+      ]
+
+      assert [%Tablespoon.Intersection.Config{}] = configs(Jason.encode!(json))
+    end
+
+    test "returns nil if no data is provided" do
+      assert configs(nil) == []
+    end
+  end
 end
