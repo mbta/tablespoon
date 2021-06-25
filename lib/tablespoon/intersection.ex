a0ceaa161131d4617bebe273acd72cdcf72226ff
Paul Swartz <paul@paulswartz.net>
2021-06-25 14:39:09 -0400

    wip: add debugging for unknown messages

--- lib/tablespoon/intersection.ex
+++ lib/tablespoon/intersection.ex
@@ -218,6 +218,13 @@ defmodule Tablespoon.Intersection do
             } pid=#{inspect(self())} message=#{inspect(message)}"
           end)
 
+        _ =
+          Logger.debug(fn ->
+            "unexpected message debug alias=#{config.alias} config=#{
+              inspect(config, limit: :infinity)
+            }"
+          end)
+
         {:noreply, state, config.warning_timeout_ms}
     end
   end
