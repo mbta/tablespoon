c405e0966501a883ebf40d17bb7b7b9618f8f8f4
Paul Swartz <paul@paulswartz.net>
2023-06-14 09:59:34 -0400

    fix: accept HTML from the error view and update the test

--- lib/tablespoon_web/views/error_view.ex
+++ lib/tablespoon_web/views/error_view.ex
@@ -11,6 +11,11 @@ defmodule TablespoonWeb.ErrorView do
   # the template name. For example, "404.json" becomes
   # "Not Found".
   def template_not_found(template, _assigns) do
-    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+    status_message = Phoenix.Controller.status_message_from_template(template)
+
+    case template do
+      <<_status::binary-3, ".json">> -> %{errors: %{detail: status_message}}
+      _ -> status_message
+    end
   end
 end
